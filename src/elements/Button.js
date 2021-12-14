812d596809e2a38cbd8cd06f8f01bada6679d4b5
hamkke <walwalzz@naver.com>
2021-12-14 14:07:45 +0900

    로그인 뷰 완성

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Button = (props) => {
-  const {_onClick, _className, version, children} = props
+  const { _onClick, _className, version, children, disabled } = props;
   const styles = {
     onClick: _onClick,
     className: _className,
-    version
-  }
+    version,
+    disabled,
+  };
   return (
-      <ButtonEl type="button" {...styles}>
-        {children}
-      </ButtonEl>
+    <ButtonEl type="button" {...styles}>
+      {children}
+    </ButtonEl>
   );
 };
 
@@ -21,23 +22,21 @@ const ButtonEl = styled.button`
   cursor: pointer;
   width: 120px;
   height: 40px;
-  border: 1px solid #D1D3D8;
+  border: 1px solid #d1d3d8;
   border-radius: 6px;
   display: flex;
   align-items: center;
   justify-content: center;
-  
-  ${
-    (props) => {
-      switch (props.version) {
-        case 'orange':
-          return 'color: #fff; background-color: var(--point-color); border-color: var(--point-color);'
-        default:
-      }
+
+  ${(props) => {
+    switch (props.version) {
+      case "orange":
+        return "color: #fff; background-color: var(--point-color); border-color: var(--point-color);";
+      default:
     }
-  }
-  
+  }}
+
   &:disabled {
     opacity: 0.6;
   }
-`
+`;
