ab6d3473f8efa801d8a8aa23094d109a23a5e6a6
hamkke <walwalzz@naver.com>
2021-12-14 18:50:19 +0900

    [DELEDTE] src/api.js 파일 삭제

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL : "http://3.34.183.214/",
-  headers: {
-    'content-type': 'application/json;charset=UTF-8',
-    accept: 'application/json,',
-  }
-});
